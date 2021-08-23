f766f72ab423a49ad63402b03a8d4e8ce7c6fc49
SHISHIRA <Shishiras98@gmail.com>
2021-08-23 15:56:20 +0530

    Added:check if lines are equal

--- LineCompare.java
+++ LineCompare.java
@@ -2,43 +2,57 @@ package linecompare;
 
 public class LineCompare {
 	
-	private int x1,y1,x2,y2;
-	/**
-	 * Assigns point coordinates 
-	 * @param x1 X coordinate of line1
-	 * @param y1 Y coordinate of line1
-	 * @param x2 X coordinate of line2
-	 * @param y2 Y coordinate of line2
-	 */
-	LineCompare(int x1,int y1,int x2,int y2){
-		this.x1=x1;
-		this.y1=y1;
-		this.x2=x2;
-		this.y2=y2;
-	}
-	/**
-	 * Displays welcome message on console
-	 */
-	private static void displayWelcome() {
-		System.out.println("Welcome to Line Comparison Computation Program");
-	}
-	/**
-	 * calculates distance between two points
-	 * @return length
-	 */
-	private double calcLength() {
-		double length;
-		length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		return length;
-	}
+		
+		private int x1,y1,x2,y2;
+		/**
+		 * Assigns point coordinates 
+		 * @param x1 X coordinate of line1
+		 * @param y1 Y coordinate of line1
+		 * @param x2 X coordinate of line2
+		 * @param y2 Y coordinate of line2
+		 */
+		LineCompare(int x1,int y1,int x2,int y2){
+			this.x1=x1;
+			this.y1=y1;
+			this.x2=x2;
+			this.y2=y2;
+		}
+		/**
+		 * Displays welcome message on console
+		 */
+		private static void displayWelcome() {
+			System.out.println("Welcome to Line Comparison Computation Program");
+		}
+		/**
+		 * calculates distance between two points
+		 * @return length
+		 */
+		private double calcLength() {
+			double length;
+			length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+			return length;
+		}
+		/**
+		 * Checks if lines have same length or not
+		 * @return true if lines have same length, otherwise false
+		 */
+		private static boolean checkEqual() {
+			LineCompare line3 = new LineCompare(1,2,3,6);
+			double length3=line3.calcLength();
+			LineCompare line4 = new LineCompare(1,2,5,6);
+			double length4=line4.calcLength();
+			if(length3==length4)
+				return true;
+			else
+				return false;
+		}
 
-	public static void main(String[] args) {
-		displayWelcome();
-		LineCompare line1=new LineCompare(1,2,3,4);
-		LineCompare line2=new LineCompare(8,4,1,1);
-		line1.calcLength();
-		line2.calcLength();
-        
-	}
-
+		public static void main(String[] args) {
+			displayWelcome();
+			LineCompare line1=new LineCompare(1,2,3,4);
+			LineCompare line2=new LineCompare(8,4,1,1);
+			line1.calcLength();
+			line2.calcLength();
+	        checkEqual();
+		}
 }
